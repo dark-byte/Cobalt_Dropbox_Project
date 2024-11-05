7f61e764d3d01d9306ace750f94c208fc13d10f1
Adrish Mitra <adrishmitra710@gmail.com>
2024-11-05 13:00:20 +0530

    cors origin changed to host uri

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://cobalt-dropbox-project.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
